0bb00c02fee7ee35ea899f54b50d0f63b9439b81
Ningomba <ningombamchingkheinganbaangom@gmail.com>
2025-04-23 02:49:39 +0530

    code add for admin layout

--- src/app/admin/admin-layout/admin-layout.component.ts
+++ src/app/admin/admin-layout/admin-layout.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { Route, Router, RouterLinkActive, RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-admin-layout',
-  imports: [],
+  imports: [ MatIconModule, RouterOutlet, RouterLinkActive, RouterModule],
+  standalone:true,
   templateUrl: './admin-layout.component.html',
   styleUrl: './admin-layout.component.scss'
 })
 export class AdminLayoutComponent {
+  constructor(private router: Router) {}
 
 }
--- src/app/user/user-layout/user-layout.component.ts
+++ src/app/user/user-layout/user-layout.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../../shared/navbar/navbar.component';
+import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from '../../shared/footer/footer.component';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-user-layout',
-  imports: [],
+  imports: [ RouterOutlet, FooterComponent,MatIconModule],
   templateUrl: './user-layout.component.html',
   styleUrl: './user-layout.component.scss'
 })
